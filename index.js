bbb8bacff62bf8c67f3e2bd37b94dce44bb23644
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-09-29 23:11:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield']
 function destructivelyAppendKitten (name){
-  name.push("kittens")
-  return name
+  kittens.push(name)
+  return kittens
 }
